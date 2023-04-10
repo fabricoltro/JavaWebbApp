4fe9e371a4162eaf2fb30f23fbfe9a77e733845a
fcoltro <fcoltro@BASIS-NTUS67.drogdelsud.com.ar>
2023-04-10 16:52:14 -0300

    asd

--- WebbApp/src/main/java/dao2/PersonaDAO.java
+++ WebbApp/src/main/java/dao2/PersonaDAO.java
@@ -1,13 +1,13 @@
 package dao2;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.mysql.jdbc.PreparedStatement;
-import com.mysql.jdbc.Statement;
+import com.mysql.cj.xdevapi.Statement;
 
 import conexion.ConnectionManager;
 import conexion.DAOException;
