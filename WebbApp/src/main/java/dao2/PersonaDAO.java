0c1ac9ae364a67556c6291ce311bf831dcef43c3
fabri_z <fabricoltro@hotmail.com>
2023-07-10 23:55:19 -0300

    primerCommit

--- WebbApp/src/main/java/dao2/PersonaDAO.java
+++ WebbApp/src/main/java/dao2/PersonaDAO.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.mysql.cj.xdevapi.Statement;
+import com.mysql.jdbc.Statement;
 
 import conexion.ConnectionManager;
 import conexion.DAOException;
